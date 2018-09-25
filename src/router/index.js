c5fbdd1830482f302bbe0a2b2c9f511d95d87bc1
中井　貴之 <uu135923@CA3919.local>
2018-09-25 19:05:38 +0900

    test: authを1回取っ払ってみる

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Login from '@/components/Login'
-import SignUp from '@/components/SignUp'
 import firebase from 'firebase'
 
 Vue.use(Router)
@@ -13,38 +11,7 @@ let router = new Router({
     {
       path: '*',
       redirect: '/hello'
-    },
-    {
-      path: '/',
-      redirect: '/hello'
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/sign-up',
-      name: 'SignUp',
-      component: SignUp
-    },
-    {
-      path: '/hello',
-      name: 'Hello',
-      component: Hello,
-      meta: {
-        requiresAuth: true
-      }
     }
   ]
-})
-
-router.beforeEach((to, from, next) => {
-  let currentUser = firebase.auth().currentUser;
-  let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-
-  if(requiresAuth && !currentUser) next('login')
-  else if(!requiresAuth && currentUser) next('hello')
-  else next()
 })
 export default router
